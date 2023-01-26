f3cc78f11706c56badf037e8a4027bf5d253a2b1
Casey <leptone55@gmail.com>
2023-01-26 08:54:18 -0800

    init

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+// export default {
+//   clearMocks: true,
+//   coverageProvider: "v8",
+//   preset: "ts-jest/presets/js-with-ts",
+//   setupFiles: ["dotenv/config"],
+//   transform: {
+//     "^.+\\.mjs?$": "ts-jest",
+//   },
+// };
+
+const config = {
+  clearMocks: true,
+  coverageProvider: "v8",
+  preset: "ts-jest/presets/js-with-ts",
+  setupFiles: ["dotenv/config"],
+  transform: {
+    "^.+\\.mjs?$": "ts-jest",
+  },
+};
+
+export default config;
